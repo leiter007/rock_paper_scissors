786d98700f32772f7ac88f705a260d1ef75931ee
felix_bonnier <felix_bonnier@yahoo.com>
2019-05-01 19:12:40 +0200

    adds unit tests for two player game class Round_2P. All tests green

--- spec.helper.js
+++ spec.helper.js
@@ -2,8 +2,12 @@ const chai = require('chai');
 const BrowserHelpers = require('e2e_training_wheels')
 global.browser = new BrowserHelpers()
 global.expect = chai.expect;
+
+// global.Round = require('./src/js/round.js');
+// global.Round_2P = require('./src/js/round_2P.js');
+
 const fs = require('fs');
 
-let round = fs.readFileSync('./src/js/round.js');
-let round2 = fs.readFileSync('./src/js/round_2P.js');
-eval(round + round2 + `\nexports.Round = Round;\nexports.Round = Round_2P;`)
+let calc = fs.readFileSync('./src/js/round.js');
+let calc_2 = fs.readFileSync('./src/js/round_2P.js');
+eval(calc + calc_2 + `\nexports.Round = Round;\nexports.Round_2P = Round_2P`)
